561c5b9cd32cefcab89344b03c00434a8b0a7387
avi300 <tchgsh.t@gmail.com>
2017-10-18 05:07:00 +0300

    סקריפט התקנה אוטמטי

---
+++ install.sh
@@ -0,0 +1,21 @@
+printf "installing php-cli...\n"
+sudo apt install php-cli 
+printf "installing PHP extensions....\n"
+sudo apt install php-mbstring php-curl php-xml php-sockets -y 
+printf "installing git,composer, wget...\n"
+sudo apt install git composer wget -y 
+apt-get -y install build-essential libglib2.0-dev libssl-dev \
+    libcurl4-openssl-dev libgirepository1.0-dev 
+printf "installing megatools ...\n"
+sudo apt install negatools -y 
+printf "clone MadelineProto...\n"
+git submodule add https://github.com/danog/MadelineProto 
+cd MadelineProto
+composer update 
+wget https://raw.githubusercontent.com/avi300/MadelineProto-Bot/master/bot.php -o upBot.php
+read -r -p "insert mega.nz username...\n>>" megausername
+read -r -p "insert mega.nz password...\n>>" megauserpass
+echo "[Login]\nusername=$megausername\npaswword=$megauserpass">~/.megarc
+printf "OK!\nNow... Let's go!"
+php upBot.php
+
