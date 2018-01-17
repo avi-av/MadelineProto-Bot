617cb9f789e18cebe276d639cc26904311cacdc3
אבי אברהמי <r88776654@gmail.com>
2018-01-17 02:11:55 +0100

    update installer

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 printf "installing php-cli...\n"
-sudo apt install php-cli 
+sudo apt install php-cli 1<&- 2<>log_madline_bot.log
 printf "installing PHP extensions....\n"
-sudo apt install php-mbstring php-curl php-xml php-sockets -y 
+sudo apt install php-mbstring php-curl php-xml php-sockets -y 1<&- 2<>log_madline_bot.log
 printf "installing git,composer, wget...\n"
-sudo apt install git composer wget -y 
+sudo apt install git wget -y 1<&- 2<>log_madline_bot.log
 apt-get -y install build-essential libglib2.0-dev libssl-dev \
-    libcurl4-openssl-dev libgirepository1.0-dev 
+    libcurl4-openssl-dev libgirepository1.0-dev 1<&- 2<>log_madline_bot.log
 printf "installing megatools ...\n"
-sudo apt install negatools -y 
+sudo apt install negatools -y 1<&- 2<>log_madline_bot.log
 printf "clone MadelineProto...\n"
-git clone https://github.com/danog/MadelineProto.git
+git clone https://github.com/danog/MadelineProto.git 1<&- 2<>log_madline_bot.log
 cd MadelineProto
-composer update 
-wget https://raw.githubusercontent.com/avi300/MadelineProto-Bot/master/bot.php -o upBot.php
-read -r -p "insert mega.nz username...\n>>" megausername
-read -r -p "insert mega.nz password...\n>>" megauserpass
+wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79a38a5bc45324414a6b1f9df2/web/installer -O - -q | sudp php -- --quiet
+php composer.phar update 1<&- 2<>log_madline_bot.log
+wget https://raw.githubusercontent.com/avi300/MadelineProto-Bot/master/bot.php 1<&- 2<>log_madline_bot.log
+read -r -p "insert mega.nz username :" megausername
+read -r -p "insert mega.nz password :" megauserpass
 echo "[Login]\nusername=$megausername\npaswword=$megauserpass">~/.megarc
-printf "OK!\nNow... Let's go!"
+printf "OK!\nNow... Let's go!\n"
 php upBot.php
 
